cec3d89f707d785dc34582b64ec8aa47ae3fefc9
LingJiang9 <D22128264@mytudublin.ie>
2024-04-08 20:51:21 +0100

    ci failed

--- main.c
+++ main.c
@@ -21,11 +21,11 @@ void print_128bit_block(unsigned char *block) {
 }
 
 int main() {
-  // unsigned char plaintext[16] = {1, 2,  3,  4,  5,  6,  7,  8,
-  //                                9, 10, 11, 12, 13, 14, 15, 16};
+  unsigned char plaintext[16] = {1, 2,  3,  4,  5,  6,  7,  8,
+                                 9, 10, 11, 12, 13, 14, 15, 16};
 
-  // unsigned char key[16] = {50, 20, 46, 86, 67, 9, 70, 27,
-  //                          75, 17, 51, 17, 4,  8, 6,  99};
+  unsigned char key[16] = {50, 20, 46, 86, 67, 9, 70, 27,
+                           75, 17, 51, 17, 4,  8, 6,  99};
 
   unsigned char *ciphertext = aes_encrypt_block(plaintext, key);
   // unsigned char *recovered_plaintext = aes_decrypt_block(ciphertext, key);
