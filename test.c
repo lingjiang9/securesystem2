3ad790f07131dfb00ae65423da08421df454defd
LingJiang9 <D22128264@mytudublin.ie>
2024-04-08 21:07:23 +0100

    test

--- test.c
+++ test.c
@@ -42,114 +42,101 @@ int main() {
 
   // Using memcmp for comparison
   if (memcmp(test_block1, expected_output1, 16) == 0) {
-      printf("Test Case 1 Passed: The output matches the expected
-      output.\n");
+    printf("Test Case 1 Passed: The output matches the expected output.\n");
   } else {
     printf(
-        "Test Case 1 Failed:testThe output does not match the expected "
-        "output.\n");
+        "Test Case 1 Failed: The output does not match the expected output.\n");
   }
 
   // +++++++++++++Test Case 2: shift rows++++++++++++
-  //   unsigned char test_block2[16] = {212, 39,  17, 174, 224, 191, 152, 241,
-  //                                    184, 180, 93, 229, 30,  65,  82,  48};
+  unsigned char test_block2[16] = {212, 39,  17, 174, 224, 191, 152, 241,
+                                   184, 180, 93, 229, 30,  65,  82,  48};
 
-  //   unsigned char expected_output2[16] = {212, 191, 93, 48,  224, 180, 82,
-  //   174,
-  //                                         184, 65,  17, 241, 30,  39,  152,
-  //                                         229};
+  unsigned char expected_output2[16] = {212, 191, 93, 48,  224, 180, 82,  174,
+                                        184, 65,  17, 241, 30,  39,  152, 229};
 
-  //   printf("Before shift_rows:\n");
-  //   print_block(test_block2, 16);
+  printf("Before shift_rows:\n");
+  print_block(test_block2, 16);
 
-  //   shift_rows(test_block2);
+  shift_rows(test_block2);
 
-  //   printf("After shift_rows:\n");
-  //   print_block(test_block2, 16);
+  printf("After shift_rows:\n");
+  print_block(test_block2, 16);
 
-  //   printf("Expected output:\n");
-  //   print_block(expected_output2, 16);
+  printf("Expected output:\n");
+  print_block(expected_output2, 16);
 
-  //   // Using memcmp for comparison
-  //   if (memcmp(test_block2, expected_output2, 16) == 0) {
-  //     printf("Test Case 2 Passed: The output matches the expected
-  //     output.\n");
-  //   } else {
-  //     printf(
-  //         "Test Case 2 Failed:testThe output does not match the expected "
-  //         "output.\n");
-  //   }
+  // Using memcmp for comparison
+  if (memcmp(test_block2, expected_output2, 16) == 0) {
+    printf("Test Case 2 Passed: The output matches the expected output.\n");
+  } else {
+    printf(
+        "Test Case 2 Failed: The output does not match the expected output.\n");
+  }
 
   // +++++++++++++Test Case 3: mix columns++++++++++++
-  //   unsigned char test_block3[16] = {212, 191, 93, 48,  224, 180, 82,  174,
-  //                                    184, 65,  17, 241, 30,  39,  152, 229};
-  //   unsigned char expected_output3[16] = {4,  102, 129, 229, 224, 203, 25,
-  //   154,
-  //                                         72, 248, 211, 122, 40,  6,   38,
-  //                                         76};
+  unsigned char test_block3[16] = {212, 191, 93, 48,  224, 180, 82,  174,
+                                   184, 65,  17, 241, 30,  39,  152, 229};
+  unsigned char expected_output3[16] = {4,  102, 129, 229, 224, 203, 25, 154,
+                                        72, 248, 211, 122, 40,  6,   38, 76};
 
-  //   printf("Before mix_columns:\n");
-  //   print_block(test_block3, 16);
+  printf("Before mix_columns:\n");
+  print_block(test_block3, 16);
 
-  //   mix_columns(test_block3);
+  mix_columns(test_block3);
 
-  //   printf("After mix_columns:\n");
-  //   print_block(test_block3, 16);
+  printf("After mix_columns:\n");
+  print_block(test_block3, 16);
 
-  //   printf("Expected output:\n");
-  //   print_block(expected_output3, 16);
+  printf("Expected output:\n");
+  print_block(expected_output3, 16);
 
-  //   // Using memcmp for comparison
-  //   if (memcmp(test_block3, expected_output3, 16) == 0) {
-  //     printf("Test Case 3 Passed: The output matches the expected
-  //     output.\n");
-  //   } else {
-  //     printf(
-  //         "Test Case 3 Failed:testThe output does not match the expected "
-  //         "output.\n");
-  //   }
+  // Using memcmp for comparison
+  if (memcmp(test_block3, expected_output3, 16) == 0) {
+    printf("Test Case 3 Passed: The output matches the expected output.\n");
+  } else {
+    printf(
+        "Test Case 3 Failed: The output does not match the expected output.\n");
+  }
 
   //   ++ ++ ++ ++ ++ ++ +Test Case 4 expand key++ ++ ++ ++ ++ ++
 
-  //   unsigned char test_block4[16] = {43,  126, 21, 22,  40, 174, 210,
-  //   166,
-  //                                    171, 247, 21, 136, 9,  207, 79, 60};
-  //   unsigned char expected_output4[176] =
-  //       {43,  126, 21,  22,  40,  174, 210, 166, 171, 247, 21,  136, 9,
-  //       207,
-  //        79,  60,  160, 250, 254, 23,  136, 84,  44,  177, 35,  163, 57,
-  //        57, 42,  108, 118, 5,   242, 194, 149, 242, 122, 150, 185, 67,
-  //        89,  53, 128, 122, 115, 89,  246, 127, 61,  128, 71,  125, 71,
-  //        22,  254, 62, 30,  35,  126, 68,  109, 122, 136, 59,  239, 68,
-  //        165, 65,  168, 82, 91,  127, 182, 113, 37,  59,  219, 11,  173,
-  //        0,   212, 209, 198, 248, 124, 131, 157, 135, 202, 242, 184, 188,
-  //        17,  249, 21,  188, 109, 136, 163, 122, 17,  11,  62,  253, 219,
-  //        249, 134, 65,  202, 0, 147, 253, 78,  84,  247, 14,  95,  95,
-  //        201, 243, 132, 166, 79, 178, 78,  166, 220, 79,  234, 210, 115,
-  //        33,  181, 141, 186, 210, 49,  43, 245, 96, 127, 141, 41,  47,
-  //        172, 119, 102, 243, 25,  250, 220, 33, 40,  209, 41,  65,  87,
-  //        92,  0,   110, 208, 20,  249, 168, 201, 238, 37,  137, 225, 63,
-  //        12,  200, 182, 99,  12,  166}
+  unsigned char test_block4[16] = {43,  126, 21, 22,  40, 174, 210, 166,
+                                   171, 247, 21, 136, 9,  207, 79,  60};
+  unsigned char expected_output4[176] =
+      {43,  126, 21,  22,  40,  174, 210, 166, 171, 247, 21,  136, 9,   207,
+       79,  60,  160, 250, 254, 23,  136, 84,  44,  177, 35,  163, 57,  57,
+       42,  108, 118, 5,   242, 194, 149, 242, 122, 150, 185, 67,  89,  53,
+       128, 122, 115, 89,  246, 127, 61,  128, 71,  125, 71,  22,  254, 62,
+       30,  35,  126, 68,  109, 122, 136, 59,  239, 68,  165, 65,  168, 82,
+       91,  127, 182, 113, 37,  59,  219, 11,  173, 0,   212, 209, 198, 248,
+       124, 131, 157, 135, 202, 242, 184, 188, 17,  249, 21,  188, 109, 136,
+       163, 122, 17,  11,  62,  253, 219, 249, 134, 65,  202, 0,   147, 253,
+       78,  84,  247, 14,  95,  95,  201, 243, 132, 166, 79,  178, 78,  166,
+       220, 79,  234, 210, 115, 33,  181, 141, 186, 210, 49,  43,  245, 96,
+       127, 141, 41,  47,  172, 119, 102, 243, 25,  250, 220, 33,  40,  209,
+       41,  65,  87,  92,  0,   110, 208, 20,  249, 168, 201, 238, 37,  137,
+       225, 63,  12,  200, 182, 99,  12,  166}
 
-  //   ;
-  //   int size = 176;
+  ;
+  int size = 176;
 
-  //   printf("Before expand the key:\n");
-  //   print_block(test_block4, 16);
+  printf("Before expand the key:\n");
+  print_block(test_block4, 16);
 
-  //   unsigned char *result4 = expand_key(test_block4);
+  unsigned char *result4 = expand_key(test_block4);
 
-  //   printf("After expand key:\n");
-  //   print_block(result4, 176);
+  printf("After expand key:\n");
+  print_block(result4, 176);
 
-  //   printf("Expected output:\n");
-  //   print_block(expected_output4, 176);
+  printf("Expected output:\n");
+  print_block(expected_output4, 176);
 
-  //   if (areCharArraysEqual(result4, expected_output4, size)) {
-  //     printf("result4 and expected_output4 are equal.\n");
-  //   } else {
-  //     printf("result4 and expected_output4 are not equal.\n");
-  //   }
+  if (areCharArraysEqual(result4, expected_output4, size)) {
+    printf("test case 4 pass:result4 and expected_output4 match.\n");
+  } else {
+    printf("test case 4 fail: result4 and expected_output4 do not match.\n");
+  }
 
   // +++++++++++++Test Case 5: add round key++++++++++++
   // Declare the function prototype
